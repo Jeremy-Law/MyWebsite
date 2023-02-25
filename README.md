26d5da97843a5329bfb9c08ef7bf102ff4f5a0c8
Jeremy L <jeremylaw2021@gmail.com>
2023-02-24 23:22:27 -0500

    Fixed buttons, changed background, Adding course work and descirptions

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # MyWebsite
 This is my first time doing web development.
-This project is for me to display and talk about all of the projects I've done in and out of school, since I can't do that on a resume. It also would violate academic intergity policies at my school to upload any school projects to Github.
+This project is for me to display and talk about all of the projects I've done in and out of school, 
+since it's too much to show on a resume. It also would violate academic integrity policies at my school to upload any school projects to Github.
--- static/JS/Home.js
+++ static/JS/Home.js
@@ -1,18 +1,18 @@
 function toProject(){
-
+    window.location.href ="Projects.html"
 }
 function toHome(){
-
+    window.location.href="Home.html"
 }
 function toAbout(){
-
+    window.location.href="aboutMe.html"
 }
 
 const pButton = document.getElementById("toProjects")
-pButton.addEventListener('click', toProject())
+pButton.addEventListener('click', toProject)
 
 const aButton = document.getElementById("toAbout")
-aButton.addEventListener('click', toAbout())
+aButton.addEventListener('click', toAbout)
 
 const hButton = document.getElementById("toHome")
 hButton.addEventListener('click', toHome)
